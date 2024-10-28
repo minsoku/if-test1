1172476ffc747fc6a59e4630642acc3ef2d98a87
minsoku <minsoku03@gmail.com>
2024-10-28 11:36:56 +0900

    1번 init

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      iframe 간 통신테스트 1번
+    </div>
+  );
+}
